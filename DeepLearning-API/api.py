d7361dee0f8acb0d9d8853537c91d9a64d3862d6
Aryan-401 <aryan.raj.garg@gmail.com>
2023-10-29 01:48:15 +0530

    API works with models

--- DeepLearning-API/api.py
+++ DeepLearning-API/api.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
+from model.model_architecture import predict
 
 app = FastAPI()
 
@@ -16,3 +16,27 @@ async def sample_query(message: str = "Hello World"):
     localhost:8000/sample_query?message=message
     """
     return {"Message": message}
+
+
+@app.post("/predict")
+async def predict_class(base64_image: dict):
+    """
+    for output of this function, use the following command:
+    localhost:8000/predict
+    """
+    try:
+        print(base64_image)
+        base64_image = base64_image.get("base64_image")
+        if not base64_image:
+            raise HTTPException(
+                status_code=400, detail="Missing 'base64_image' in the request body"
+            )
+        top_p, top_classes = predict(
+            image_string=base64_image,
+            topk=3,
+            train_on_gpu=False,
+            n_classes=14,
+        )
+        return {"top_classes": top_classes}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
