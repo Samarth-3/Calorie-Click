bbb078c4659190a2527c503452a7975dabdea9a5
Aryan-401 <aryan.raj.garg@gmail.com>
2023-11-08 23:37:10 +0530

    Added 14 Food nutritional information

--- Frontend/src/Data.js
+++ Frontend/src/Data.js
@@ -1,35 +1,89 @@
 const foodData = [
     {
-      name: "Samosas",
-      protein: 4.5, // Protein content in grams
-      carbs: 22,   // Carbohydrate content in grams
-      fiber: 2.1   // Dietary fiber content in grams
+        name: "Samosas",
+        protein: 4.5, // Protein content in grams
+        carbs: 22,   // Carbohydrate content in grams
+        fiber: 2.1   // Dietary fiber content in grams
     },
     {
-      name: "VadaPav",
-      protein: 6.8,
-      carbs: 51,
-      fiber: 3.2
+        name: "VadaPav",
+        protein: 6.8,
+        carbs: 51,
+        fiber: 3.2
     },
     {
-      name: "Chole Bhature",
-      protein: 14.3,
-      carbs: 58,
-      fiber: 7.5
+        name: "CholeBhature",
+        protein: 14.3,
+        carbs: 58,
+        fiber: 7.5
     },
     {
-      name: "PaniPuri",
-      protein: 21.3,
-      carbs: 58,
-      fiber: 7.5
+        name: "PaniPuri",
+        protein: 21.3,
+        carbs: 58,
+        fiber: 7.5
     },
     {
-      name: "Jalebionplate",
-      protein: 14.3,
-      carbs: 52,
-      fiber: 7.5
+        name: "Jalebionplate",
+        protein: 14.3,
+        carbs: 52,
+        fiber: 7.5
+    },
+    {
+        name: "AlooParatha",
+        protein: 5,
+        carbs: 35,
+        fiber: 3
+    },
+    {
+        name: "Biryani",
+        protein: 20,
+        carbs: 31,
+        fiber: 1.4
+    },
+    {
+        name: "ButterChicken",
+        protein: 26.5,
+        carbs: 12.8,
+        fiber: 1.7
+    },
+    {
+        name: "DalMakhani",
+        protein: 8.5,
+        carbs: 24,
+        fiber: 6.9
+    },
+    {
+        name: "Dosa",
+        protein: 3.9,
+        carbs: 29,
+        fiber: 0.9
+    },
+    {
+        name: "GulabJamuninbowl",
+        protein: 2,
+        carbs: 54.4,
+        fiber: 0.6
+    },
+    {
+        name: "Idli", // stats for 3 idli
+        protein: 4.8,
+        carbs: 0.1,
+        fiber: 36
+    },
+    {
+        name: "Naan",
+        protein: 9,
+        carbs: 45,
+        fiber: 2
+    },
+    {
+        name: "PaneerTikka",
+        protein: 17,
+        carbs: 14.3,
+        fiber: 0.7
     }
-  ];
-  
-  export default foodData;
+];
+
+export default foodData;
   
