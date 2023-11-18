0547a63d9b94b59c7887b4bd04aacbaabac0a6f6
Samarth-3 <spaliwal_be21@thapar.edu>
2023-11-18 11:25:00 +0530

    minor error fixed

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -94,8 +94,6 @@ const Login = () => {
           }}/>
           <Button onClick={handleClick} disabled={isFetching}>LOGIN</Button>
           {error && <Error>Something went wrong...</Error>}
-          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
-          <Link>CREATE A NEW ACCOUNT</Link>
         </Form>
       </Wrapper>
     </Container>
